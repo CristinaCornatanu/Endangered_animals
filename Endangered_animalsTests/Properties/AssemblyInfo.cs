f9e9d728f3d7b8119c22274cf68508cc8407f9de
CristinaCornatanu20 <cornateanucristina@gmail.com>
2024-04-15 14:53:32 +0300

    V1.6. Add UnitTestProject, remove unit test file

---
+++ Endangered_animalsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Endangered_animalsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Endangered_animalsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c7e4ac7-bb76-481c-bf9a-19a3105654a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Endangered_animals;
+using Endangered_animals.Presentation;
+using Endangered_animals.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConvertImageToByteArray_Success()
+        {
+            Image testImage = Image.FromFile("C:\\Users\\corna\\Downloads\\Danaus_plexippus_MHNT.jpg");
+
+            byte[] result = ImageConverterHelper.ConvertImageToByteArray(testImage);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length > 0);
+        }
+
+        [TestMethod]
+        public void TestConvertByteArrayToImage()
+        {
+
+            byte[] testByteArray = File.ReadAllBytes("C:\\Users\\corna\\Downloads\\Danaus_plexippus_MHNT.jpg");
+
+            Image result = ImageConverterHelper.ConvertByteArrayToImage(testByteArray);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Image));
+            Assert.IsTrue(result.Width > 0 && result.Height > 0);
+        }
+
+      
+    }
+}
